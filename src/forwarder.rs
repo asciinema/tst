3f4910ac277623eefb39a2fd690646a9dbf783f8
Marcin Kulik <m@ku1ik.com>
2023-07-03 17:28:25 +0200

    Extract WS forwarder into its own module

---
+++ src/forwarder.rs
@@ -0,0 +1,61 @@
+use crate::{alis_stream, ClientInitRequest};
+use anyhow::Result;
+use futures_util::{sink, stream, StreamExt};
+use log::{debug, info};
+use std::time;
+use tokio::sync::mpsc;
+use tokio_stream::wrappers::IntervalStream;
+
+pub async fn forward(clients_tx: mpsc::Sender<ClientInitRequest>, url: url::Url) -> Result<()> {
+    let mut reconnect_attempt = 0;
+
+    loop {
+        let time = time::Instant::now();
+        let result = forward_once(&clients_tx, &url).await;
+        debug!("forwarder: {:?}", &result);
+
+        if time.elapsed().as_secs_f32() > 1.0 {
+            reconnect_attempt = 0;
+        }
+
+        let delay = exponential_delay(reconnect_attempt);
+        reconnect_attempt = (reconnect_attempt + 1).min(10);
+        info!("forwarder: connection closed, reconnecting in {delay}");
+        tokio::time::sleep(time::Duration::from_millis(delay)).await;
+    }
+}
+
+const WS_PING_INTERVAL: u64 = 15;
+
+async fn forward_once(clients_tx: &mpsc::Sender<ClientInitRequest>, url: &url::Url) -> Result<()> {
+    use tokio_tungstenite::tungstenite;
+
+    let (ws, _) = tokio_tungstenite::connect_async(url).await?;
+    info!("forwarder: connected to endpoint");
+    let (write, read) = ws.split();
+
+    tokio::spawn(async {
+        let _ = read.map(Ok).forward(sink::drain()).await;
+    });
+
+    let alis_stream = alis_stream(clients_tx)
+        .await?
+        .map(tungstenite::Message::binary);
+
+    let interval = tokio::time::interval(time::Duration::from_secs(WS_PING_INTERVAL));
+
+    let ping_stream = IntervalStream::new(interval)
+        .skip(1)
+        .map(|_| tungstenite::Message::Ping(vec![]));
+
+    stream::select(alis_stream, ping_stream)
+        .map(Ok)
+        .forward(write)
+        .await?;
+
+    Ok(())
+}
+
+fn exponential_delay(attempt: usize) -> u64 {
+    (2_u64.pow(attempt as u32) * 500).min(5000)
+}
--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,21 @@ use anyhow::{bail, Result};
 use avt::Vt;
 use clap::{ArgEnum, Parser};
 use env_logger::Env;
-use futures_util::{sink, stream, Stream, StreamExt};
+use futures_util::{stream, Stream, StreamExt};
 use log::{debug, info};
 use regex::Regex;
 use serde::Deserialize;
 use std::future;
 use std::net::SocketAddr;
 use std::pin::Pin;
-use std::time;
 use tokio::fs::File;
 use tokio::io::{AsyncBufReadExt, AsyncRead, AsyncReadExt};
 use tokio::sync::{broadcast, mpsc, oneshot};
 use tokio::time::Instant;
-use tokio_stream::wrappers::{BroadcastStream, IntervalStream};
+use tokio_stream::wrappers::BroadcastStream;
 mod alis;
+mod forwarder;
 mod server;
-
-const WS_PING_INTERVAL: u64 = 15;
 
 #[derive(Clone, Debug, ArgEnum)]
 enum InputFormat {
@@ -223,58 +221,6 @@ async fn read_file(
     Ok(())
 }
 
-async fn forward(clients_tx: &mpsc::Sender<ClientInitRequest>, url: &url::Url) -> Result<()> {
-    use tokio_tungstenite::tungstenite;
-
-    let (ws, _) = tokio_tungstenite::connect_async(url).await?;
-    info!("forwarder: connected to endpoint");
-    let (write, read) = ws.split();
-
-    tokio::spawn(async {
-        let _ = read.map(Ok).forward(sink::drain()).await;
-    });
-
-    let alis_stream = alis_stream(clients_tx)
-        .await?
-        .map(tungstenite::Message::binary);
-
-    let interval = tokio::time::interval(time::Duration::from_secs(WS_PING_INTERVAL));
-
-    let ping_stream = IntervalStream::new(interval)
-        .skip(1)
-        .map(|_| tungstenite::Message::Ping(vec![]));
-
-    stream::select(alis_stream, ping_stream)
-        .map(Ok)
-        .forward(write)
-        .await?;
-
-    Ok(())
-}
-
-fn exponential_delay(attempt: usize) -> u64 {
-    (2_u64.pow(attempt as u32) * 500).min(5000)
-}
-
-async fn forwarder(clients_tx: mpsc::Sender<ClientInitRequest>, url: url::Url) -> Result<()> {
-    let mut reconnect_attempt = 0;
-
-    loop {
-        let time = time::Instant::now();
-        let result = forward(&clients_tx, &url).await;
-        debug!("forwarder: {:?}", &result);
-
-        if time.elapsed().as_secs_f32() > 1.0 {
-            reconnect_attempt = 0;
-        }
-
-        let delay = exponential_delay(reconnect_attempt);
-        reconnect_attempt = (reconnect_attempt + 1).min(10);
-        info!("forwarder: connection closed, reconnecting in {delay}");
-        tokio::time::sleep(time::Duration::from_millis(delay)).await;
-    }
-}
-
 #[derive(Debug)]
 pub struct ClientInitResponse {
     online: bool,
@@ -420,7 +366,7 @@ async fn main() -> Result<()> {
 
     if let Some(url) = cli.forward_url {
         info!("forwarding to {}", &url);
-        tokio::spawn(forwarder(clients_tx, url));
+        tokio::spawn(forwarder::forward(clients_tx, url));
     }
 
     tokio::spawn(handle_events(cli.cols, cli.rows, input_rx, clients_rx));
